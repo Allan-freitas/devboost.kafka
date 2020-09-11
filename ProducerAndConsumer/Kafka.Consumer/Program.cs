a17efee850407375bab9a404a3094c5e936f8d83
allansud <freitasallan@gmail.com>
2020-09-11 20:32:16 -0300

    Subindo...

---
+++ ProducerAndConsumer/Kafka.Consumer/Program.cs
@@ -0,0 +1,50 @@
+﻿using Confluent.Kafka;
+using System;
+using System.Threading;
+
+namespace Kafka.Consumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var config = new ConsumerConfig
+            {
+                GroupId = "test-consumer-group",
+                BootstrapServers = "192.168.88.48:9092",
+                AutoOffsetReset = AutoOffsetReset.Earliest,
+            };
+
+            using var c = new ConsumerBuilder<Ignore, string>(config).Build();
+            c.Subscribe("devboost");
+
+            CancellationTokenSource cts = new CancellationTokenSource();
+            Console.CancelKeyPress += (_, e) =>
+            {
+                e.Cancel = true; // prevent the process from terminating.
+                    cts.Cancel();
+            };
+            try
+            {
+                while (true)
+                {
+                    try
+                    {
+                        var cr = c.Consume(cts.Token);
+
+                        Console.WriteLine($"Consumed message '{cr.Message}' at: '{cr.TopicPartitionOffset}'.");
+                    }
+                    catch (ConsumeException e)
+                    {
+                        Console.WriteLine($"Error occurred: {e.Error.Reason}");
+
+                    }
+                }
+            }
+            catch (OperationCanceledException)
+            {
+                // Ensure the consumer leaves the group cleanly and final offsets are committed.
+                c.Close();
+            }
+        }
+}
---
+++ ProducerAndConsumer/Kafka.Producer/Program.cs
@@ -0,0 +1,60 @@
+﻿using Confluent.Kafka;
+using Serilog;
+using Serilog.Core;
+using System;
+using System.Threading.Tasks;
+
+namespace Kafka.Producer
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Logger logger = new LoggerConfiguration()
+                .WriteTo.Console()
+                .CreateLogger();
+
+            logger.Information("Testando o envio de mensagens com Kafka");
+
+            const string bootstrapServers = "192.168.88.48:9092";
+            const string nomeTopic = "devboost";
+            string burroCarga = $"Mensagem => {Guid.NewGuid()} Hora => {DateTime.Now}";
+
+            logger.Information($"BootstrapServers = {bootstrapServers}");
+            logger.Information($"Topic = {nomeTopic}");
+
+            try
+            {
+                ProducerConfig config = new ProducerConfig
+                {
+                    BootstrapServers = bootstrapServers
+                };
+
+                using (IProducer<Null, string> producer = new ProducerBuilder<Null, string>(config).Build())
+                {
+                    for (int i = 2; i < args.Length; i++)
+                    {
+                        var result = await producer.ProduceAsync(
+                            nomeTopic,
+                            new Message<Null, string>
+                            { Value = burroCarga });
+
+                        logger.Information(
+                            $"Mensagem: {args[i]} | " +
+                            $"Status: { result.Status}");
+                    }
+                }
+
+                logger.Information("Jegue/Burro despachado");
+                Console.WriteLine("Jegue/Burro despachado");
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
+
